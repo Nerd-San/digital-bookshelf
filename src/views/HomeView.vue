b507a4dc224f736883a6763dbaf0fe72c19ea8c8
Dmitri Carapezza <dmitricarapezza@gmail.com>
2023-09-17 16:01:08 -0600

    Add todos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,5 +3,10 @@
 <template>
   <main>
     <h1>What up my goats</h1>
+    <h2 class="todos">Todos:</h2>
+    <ul>
+      <li>Read a barcode with the camera</li>
+      <li>Look up ISBN codes for books from an API</li>
+    </ul>
   </main>
 </template>
